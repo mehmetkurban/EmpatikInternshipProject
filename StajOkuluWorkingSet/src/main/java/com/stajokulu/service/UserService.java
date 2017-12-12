5669f648fe3e892f6485e4945abea092357a0e4d
mehmetkurban <mehmetkurban@yahoo.com>
2017-12-12 18:42:36 +0200

    Update UserService.java

--- StajOkuluWorkingSet/src/main/java/com/stajokulu/service/UserService.java
+++ StajOkuluWorkingSet/src/main/java/com/stajokulu/service/UserService.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.stajokulu.service;
 
 import com.stajokulu.siteuser.SiteUser;
 import java.util.List;
 import javax.ejb.Local;
 
-/**
- *
- * @author Burakhan
- */
+
 @Local
 public interface UserService {
 
